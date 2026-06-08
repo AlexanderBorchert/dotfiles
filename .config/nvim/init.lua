20126eb55012534a2db887f1345cd52985dbd821
Alexander Borchert <alexander.he.borchert@googlemail.com>
2026-06-08 14:17:41 +0200

    nvim - update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,6 +166,9 @@ vim.o.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+--
+-- execute Makefile
+vim.keymap.set('n', '<leader>m', ':w | make<CR>', { silent = true, desc = '[M]ake execution' })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
